6dfa33d6743f208b2c3442ee08fefaf6fa5b44a6
abdalla morsy <abdallahmorsy@hotmail.com>
2023-08-20 18:52:42 -0700

    printf

--- 1_printf.c
+++ 1_printf.c
@@ -1,42 +1,52 @@
 #include "main.h"
 
+/**
+ * _printf - prints and input into the standard output
+ * @format: the format string
+ * Return: number of bytes printed
+ */
+
 int _printf(const char *format, ...)
+
 {
-	int i, count = 0;
-	va_list args;
-	char *p, *percent;
+	int sum = 0;
+	va_list arg;
+	char *p, *start;
 
-	va_start(args, format);
+	params_t myparams = PARAMS_INIT;
 
-	for (p = format; *p; p++)
+	va_start(arg, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
 	{
-		if (*p == '%')
+		init_params(&myparams, arg);
+		if (*p != '%')
 		{
-			percent = p;
-			p++;
-
-			switch (*p)
-			{
-				case 'c':
-					count += _putchar(va_arg(args, int));
-					break;
-				case 's':
-					count += _puts(va_arg(args, char *));
-					break;
-				case '%':
-					count += _putchar('%');
-					break;
-			}
-
+			sum += _putchar(*p);
+			continue;
+		}
+		start = p;
+		p++;
+		while (get_flag(p, &myparams))
+		{
 			p++;
 		}
+		p = get_width(p, &myparams, arg);
+		p = get_precision(p, &myparams, arg);
+		if (get_modifier(p, &myparams))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+					myparameters.l_modifier || myparameters.h_modifier ? p - 1 : 0);
 		else
-		{
-			count += _putchar(*p);
-		}
+			sum += get_print_func(p, arg, &myparams);
 	}
-
-	va_end(args);
-
-	return count;
+	_putchar(BUF_FLUSH);
+	va_end(arg);
+	return (sum);
 }
+
