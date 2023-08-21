3e5edc30582d3fdb6fdc749a6e680da50d93bce1
abdalla morsy <abdallahmorsy@hotmail.com>
2023-08-20 18:48:46 -0700

    printf

--- 1_printf.c
+++ 1_printf.c
@@ -1,52 +1,42 @@
 #include "main.h"
 
-/**
- * _printf - prints and input into the standard output
- * @format: the format string
- * Return: number of bytes printed
- */
-
 int _printf(const char *format, ...)
-
 {
-	int sum = 0;
-	va_list arg;
-	char *p, *start;
+	int i, count = 0;
+	va_list args;
+	char *p, *percent;
 
-	params_t myparams = PARAMS_INIT;
+	va_start(args, format);
 
-	va_start(arg, format);
-
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = (char *)format; *p; p++)
+	for (p = format; *p; p++)
 	{
-		init_params(&myparams, arg);
-		if (*p != '%')
+		if (*p == '%')
 		{
-			sum += _putchar(*p);
-			continue;
-		}
-		start = p;
-		p++;
-		while (get_flag(p, &myparams))
-		{
+			percent = p;
 			p++;
-		}
-		p = get_width(p, &myparams, arg);
-		p = get_precision(p, &myparams, arg);
-		if (get_modifier(p, &myparams))
-			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-					myparams.l_modifier || myparams.h_modifier ? p - 1 : 0);
-		else
-			sum += get_print_func(p, arg, &myparams);
-	}
-	_putchar(BUF_FLUSH);
-	va_end(arg);
-	return (sum);
-}
 
+			switch (*p)
+			{
+				case 'c':
+					count += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					count += _puts(va_arg(args, char *));
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+			}
+
+			p++;
+		}
+		else
+		{
+			count += _putchar(*p);
+		}
+	}
+
+	va_end(args);
+
+	return count;
+}
